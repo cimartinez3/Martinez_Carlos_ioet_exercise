3af25ceea0af8c1adfc1499fd7d6d219226b54c0
Carlos Martinez <carlos-m3@hotmail.com>
2021-05-15 16:18:43 -0500

    Project Structure

---
+++ MartínezCarlos_Ioet_exercise/MartínezCarlos_Ioet_exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MartínezCarlos_Ioet_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
